b2f2381f01a525a2bd186fdfd17fe005a3dd23a8
Çınar Özgürgil <cozgurgil@gmail.com>
2025-07-25 17:41:26 +0300

    Veri-Tabanı-Bağlantısı-Testi

--- app/Controllers/Front/HomeController.php
+++ app/Controllers/Front/HomeController.php
@@ -3,14 +3,27 @@
 namespace App\Controllers\Front;
 
 use App\Core\BaseController;
+use App\Core\Database;
+use Exception;
 
 class HomeController extends BaseController {
     public function index() 
     {
+
+        // Veri Tabanı Bağlantısı Testi
+
+        // try {
+        //     $db = Database::getInstance()->getConnection();
+        //     echo "Veri Tabanı Bağlantısı Başarılı.";
+        // } catch (Exception $e) {
+        //     echo "Veri tabanı bağlantısı başarısız: " . $e->getMessage();
+        // }
+
+
         // Bu aşşağıda ki alana data'dan çektiğimiz değerler olarak görebiliriz bu değerleri de view's içerisine aşşağıdaki şekilde göndeririz.
-        $title = "MVC Eğitimine Hoşgeldiniz";
-        $content = "MVC Eğitimi Detayı";
-        $this->render("front/example", ['title' => $title , 'content' => $content]);
+        // $title = "MVC Eğitimine Hoşgeldiniz";
+        // $content = "MVC Eğitimi Detayı";
+        // $this->render("front/example", ['title' => $title , 'content' => $content]);
     }
 }
 
--- app/Core/Database.php
+++ app/Core/Database.php
@@ -13,13 +13,12 @@ class Database {
     private $connection;
 
     private function __construct(){
-        $config = require __DIR__ . '/../config.php'; // Burada config dosyasını içeri dahil etmiş olduk.
+        $config = require __DIR__ . '/../../config.php'; // Burada config dosyasını içeri dahil etmiş olduk.
         $db = $config['db']; // Burada da dahil ettiğimiz sınıf içerisinden değer çekmiş olduk.
 
-        $dsn = "mysql:host={$db['host']};dbname={$dbname['dbname']};charset={$db['charset']}";
+        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset={$db['charset']};";
 
         try {
-
             $this->connection = new PDO($dsn, $db['user'], $db['password']); // Burada connection değerini bağlanmak istediğimiz değere , kullanıcı adına ve parolasına bağlarız.
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Burada setAttribute değeri PDO'nun bir hata oluştuğu PDOException değerine bunu fırlatmasını sağlar biz de burada neleri fırlatıcağını içine yazarız.
         } catch (PDOException $e) {
